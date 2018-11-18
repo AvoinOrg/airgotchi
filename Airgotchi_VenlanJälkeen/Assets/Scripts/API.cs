2917eda852052f85cb3ba12f2608e949b71cc56d
Eero Kuurne <eero.kuurne@hotmail.com>
2018-11-18 12:27:13 +0200

    Initial project

---
+++ Airgotchi_VenlanJälkeen/Assets/Scripts/API.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+/*ToDO text string floatiksi ja joksuks piste logiikaksi. Toi ulostuleva arvo kerrotaan 
+1 000 000 jollon arvo yli 40 on huono.
+Jotain kuivaa tietoa aiheesta: 
+http://www.who.int/news-room/fact-sheets/detail/ambient-(outdoor)-air-quality-and-health
+
+Mä en tiedä mitä teen, peace and love @venla oli eka kerta Unitylla, jos joku on rikki niin ehkä 
+kandee palata siihen aikasempaan zip fileen */
+
+public class API : MonoBehaviour {
+
+    private const string URL = "https://button.kunnas.com/query_no2?latitude=37.7749&longitude=122.4194&duration=1";
+    public Text responseText;
+
+    void Start() {
+        Request();
+    }
+
+    public void Request()
+    {
+        WWW request = new WWW(URL);
+        StartCoroutine(OnResponse(request));
+    }
+
+    private IEnumerator OnResponse(WWW req)
+    {
+
+        yield return req;
+
+        if (req.text != "{\"error\": \"No data\"}") {
+            responseText.text = req.text;
+        }
+    }
+}
+
+
---
+++ Airgotchi_VenlanJälkeen/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameLogic : MonoBehaviour
+{
+  [SerializeField] Sprite egg;
+  [SerializeField] Sprite egg_hatched;
+  [SerializeField] Sprite happy;
+  [SerializeField] Sprite sad;
+  [SerializeField] Sprite angry;
+
+  [SerializeField] Image  target;
+
+  int status = 0;
+
+
+  void Start()
+  {
+  }
+
+  void Update()
+  {
+  }
+
+  public void Tap()
+  {
+    if      (status == 0) target.sprite = egg_hatched;
+    else if (status == 1) target.sprite = happy;
+
+    ++status;
+  }
+}
